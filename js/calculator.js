a296e1194c91cd76160721169c70c24cc26cb049
Katrin Marschner <katrin.marschner@gmail.com>
2016-01-04 15:07:30 +0100

    make numbers/dot clickable, show to user

--- js/calculator.js
+++ js/calculator.js
@@ -3,9 +3,18 @@ $(document).ready(function() {
   var squares = [];
   for (i = 0; i <= 9; i++) {
     var j = 9 - i;
-    squares.push('<div class="square ' + j + '">' + j + '</div>');
+    squares.push('<div class="square number ' + j + '">' + j + '</div>');
   }
-  squares.push('<div class="square comma">,</div>');
+  squares.push('<div class="square dot">.</div>');
   squares.push('<div class="square minus-plus-sign">-/+</div>');
   $('.container').append(squares.join(''));
+
+  // store clicked numbers/dot and add to input field
+  var input = [];
+  $('.number, .dot').on('click', function(){    // edit this to add only one dot
+    var number = $(this).text();
+    input.push(number);
+    $('.input').empty();
+    $('.input').append(input.join(''));
+  });
 });
