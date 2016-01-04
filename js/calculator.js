0ecf764c8d273a1e86c8b4730179a91d346c0d23
Katrin Marschner <katrin.marschner@gmail.com>
2016-01-04 17:23:58 +0100

    simple operations work

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  // initial grid (9 divs)
-  var squares = [];
+  // numbers (0 - 9, ., -/+)
+  var numbers = [];
   for (i = 0; i <= 9; i++) {
     var j = 9 - i;
-    squares.push('<div class="square number ' + j + '">' + j + '</div>');
+    numbers.push('<div class="square number ' + j + '">' + j + '</div>');
   }
-  squares.push('<div class="square dot">.</div>');
-  squares.push('<div class="square minus-plus-sign">-/+</div>');
-  $('.container').append(squares.join(''));
+  numbers.push('<div class="square dot">.</div>');
+  numbers.push('<div class="square minus-plus-sign">-/+</div>');
+  $('.numbers').append(numbers.join(''));
 
   // store clicked numbers/dot and add to input field
   var input = [];
@@ -15,6 +15,50 @@ $(document).ready(function() {
     var number = $(this).text();
     input.push(number);
     $('.input').empty();
-    $('.input').append(input.join(''));
+    $('.input').text(input.join(''));
+  });
+
+  // operations
+  var num1 = 0;
+  var operation = '';
+  $('.plus, .minus, .divide, .multiply').on('click', function(){    // edit this to add only one dot
+    operation = $(this).text();
+    num1 = Number(input.join(''));
+    $('.input').text(operation);
+    input = [];
+  });
+
+  // clear
+  $('.clear').on('click', function() {
+    $('.input').empty();
+    input = [];
+    operation = '';
+    num1 = 0;
+  });
+
+  // calculate
+  var calculate = function(num1, num2) {
+    switch(operation) {
+      case '+':
+        return (num1 + num2);
+        break;
+      case '-':
+        return (num1 - num2);
+        break;
+      case '*':
+        return (num1 * num2);
+        break;
+      case '/':
+        return (num1 / num2);
+        break;
+      default:
+        return ('Error')
+    }
+  };
+
+  $('.equal').on('click', function() {
+    var num2 = Number(input.join(''));
+    var result = calculate(num1, num2);
+    $('.input').text(result);
   });
 });
