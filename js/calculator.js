900a932fd8e4972f19edea0de4515707445bc58d
Katrin Marschner <katrin.marschner@gmail.com>
2016-03-21 16:51:29 +0100

    add chaining and some more improvements

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
   // a little animation
-  $('.square').mouseenter(function(){
-    $(this).fadeTo('fast', 0.75);
+  $('.square').mouseenter(function() {
+    $(this).fadeTo('fast', 0.9);
   });
-  $('.square').mouseleave(function(){
+  $('.square').mouseleave(function() {
     $(this).fadeTo('fast', 1);
+  });
+
+  $('.square').on('click', function() {
+    $(this).css("background", "#FEDCB6");
+    setTimeout(function(){
+      $('.square').css("background", "#bbb");
+    }, 50);
+  });
+
+  // store last button clicked
+  var lastClicked = "";
+  var nowClicked = "";
+  $('.square').on('click', function() {
+    lastClicked = nowClicked;
+    nowClicked = $(this).text();
   });
 
   // store clicked numbers and add to input field
   var input = [];
-  $('.number').on('click', function(){
+  $('.number').on('click', function() {
+    if (lastClicked === "=") {
+      input = [];
+    }
     var number = $(this).text();
     input.push(number);
     $('.input').empty();
@@ -17,7 +35,7 @@ $(document).ready(function() {
   });
 
   // dot
-  $('.dot').on('click', function(){
+  $('.dot').on('click', function() {
     if (!input.includes(".")) {
       input.push(".");
     }
@@ -26,7 +44,7 @@ $(document).ready(function() {
   });
 
   // +/-
-  $('.minus-plus-sign').on('click', function(){
+  $('.minus-plus-sign').on('click', function() {
     if (input.includes("-")) {
       input.shift();
     } else {
@@ -37,33 +55,32 @@ $(document).ready(function() {
   });
 
   // Percent
-  $('.percent').on('click', function(){
-    var num = Number(input.join(''))/100;
+  $('.percent').on('click', function() {
+    var num = Number(input.join('')) / 100;
     $('.input').text(num);
     input = [num];
   });
 
-  // clear
-  $('.clear').on('click', function() {
-    $('.input').empty();
-    input = [];
-    operation = '';
-    num1 = 0;
-  });
-
   // operations
-  var num1 = 0;
-  var operation = '';
-  $('.plus, .minus, .divide, .multiply').on('click', function(){    // edit this to add only one dot
-    operation = $(this).text();
-    num1 = Number(input.join(''));
+  var chaining = [];
+  $('.plus, .minus, .divide, .multiply').on('click', function() {
+    var operation = $(this).text();
+    var number = Number(input.join(''));
     $('.input').text(operation);
+
+    // no number inbetween operations
+    if (input.join('') === '') {
+      chaining[chaining.length - 1] = operation;
+    } else {
+      chaining.push(number);
+      chaining.push(operation);
+    }
     input = [];
   });
 
   // calculate
-  var calculate = function(num1, num2) {
-    switch(operation) {
+  var calculate = function(num1, num2, opn) {
+    switch (opn) {
       case '+':
         return (num1 + num2);
         break;
@@ -82,11 +99,43 @@ $(document).ready(function() {
   };
 
   $('.equal').on('click', function() {
-    var num2 = Number(input.join(''));
-    var result = calculate(num1, num2);
+    var result = 0;
+    var number = Number(input.join(''));
+    chaining.push(number);
+
+    while (chaining.length > 1) {
+      if (chaining.includes("×")) {
+        var o = "×";
+        var n = chaining[chaining.indexOf("×") - 1];
+        var m = chaining[chaining.indexOf("×") + 1];
+        chaining[chaining.indexOf("×") - 1] = calculate(n, m, o);
+        chaining.splice(chaining.indexOf("×"), chaining.indexOf("×") + 1);
+
+      } else if (chaining.includes("÷")) {
+        var o = "÷";
+        var n = chaining[chaining.indexOf("÷") - 1];
+        var m = chaining[chaining.indexOf("÷") + 1];
+        result = calculate(n, m, o);
+        chaining[chaining.indexOf("÷") - 1] = result;
+        chaining.splice(chaining.indexOf("÷"), chaining.indexOf("÷") + 1);
+
+      } else {
+        result = calculate(chaining[0], chaining[2], chaining[1]);
+        chaining[0] = result;
+        chaining.splice(1, 2);
+      }
+    }
+    result = chaining[0];
+
     $('.input').text(result);
     input = [result];
-    operation = '';
-    num1 = 0;
+    chaining = [];
+  });
+
+  // clear
+  $('.clear').on('click', function() {
+    $('.input').empty();
+    input = [];
+    chaining = [];
   });
 });
